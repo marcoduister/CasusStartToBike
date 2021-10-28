bafc7f997807344fc09df40a1b4b9ee9aa1c5e72
Colin Bergh <1875485bergh@zuyd.nl>
2021-10-28 16:24:15 +0200

    Login/Register 1.0

---
+++ CasusStartToBike/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasusStartToBike.Models
+{
+    public class Enums
+    {
+        public enum Rollen { User, Admin }
+    }
+}
